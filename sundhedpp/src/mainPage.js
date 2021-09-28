548d72010cc2b7160ee4f9bc3b955861d93ea6fc
Sebastian Bjerre <sebastian.bjerre@pm.me>
2021-09-28 09:50:05 +0200

    renamed mainpage.js to mainPage.js

---
+++ sundhedpp/src/mainPage.js
@@ -0,0 +1,16 @@
+
+
+function MainPage() {
+    return (
+        <div className="LoginPage">
+            <header className="LoginPage-header">
+                <title>Main page </title>
+                <h1> This is the main page</h1>
+
+            </header>
+        </div>
+    );
+
+}
+
+export default MainPage;
