2215587eabf57f3441cac48de822f4d49f53b4ec
gustav <kirkholtgustav@gmail.com>
2021-10-27 17:11:12 +0200

    Search bar med filter

--- web/src/pages/Search.js
+++ web/src/pages/Search.js
@@ -1,12 +1,55 @@
-import '../styling/Search.css'
-import {searchstore} from "../stores/SearchStore";
+import React, {useState} from "react"
+import './Search.css';
+import SearchIcon from '@material-ui/icons/Search';
+import CloseIcon from '@material-ui/icons/Close';
+import PatientData from "./Data.json";
 
-const Search = () => {
-    return (
-        <div>
-            Search page
+
+function Search() {
+    const data = PatientData
+    const [filteredData, setFilteredData] = useState([]);
+    const [wordEntered, setWordEntered] = useState("");
+    const handleFilter = (event) => {
+        const searchWord = event.target.value
+        setWordEntered(searchWord);
+        const newFilter = data.filter((value) => {
+            return value.navn.toLowerCase().includes(searchWord.toLowerCase());
+        });
+
+        if(searchWord === "") {
+            setFilteredData([])
+        } else {
+            setFilteredData(newFilter);
+        }
+    };
+
+    const clearInput = () => {
+        setFilteredData([]);
+        setWordEntered("");
+    }
+    return(
+        <div className="search">
+            <div class="centered" className="searchInput" >
+                <input type="text" placeholder="Indtast patient navn..."
+                       value={wordEntered}
+                       onChange={handleFilter}/>
+                <div className="searchIcon">
+                    {filteredData.length === 0 && wordEntered.length === 0 ? <SearchIcon/> :
+                        <CloseIcon id="clearBtn" onClick={clearInput}/> }
+                </div>
+            </div>
+            {filteredData.length != 0 && (
+                <div className="dataResult">
+                    {filteredData.slice(0,15).map((value, key) => {
+                        return <a className="dataItem">
+                            <p>{value.navn}, {value.cpr} </p>
+                        </a>
+                    })}
+                </div>
+            )}
         </div>
     )
 }
 
-export default Search
+
+export default Search;
