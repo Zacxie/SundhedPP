d81659fe1ef1398a6bea6f2b63e808e533e97317
gustav <kirkholtgustav@gmail.com>
2021-10-27 20:28:13 +0200

    Fjernet gammel kode

--- web/src/pages/Search.js
+++ web/src/pages/Search.js
@@ -8,27 +8,7 @@ import Autocomplete from '@mui/material/Autocomplete';
 
 
 function Search() {
-    const data = PatientData
-    const [filteredData, setFilteredData] = useState([]);
-    const [wordEntered, setWordEntered] = useState("");
-    const handleFilter = (event) => {
-        const searchWord = event.target.value
-        setWordEntered(searchWord);
-        const newFilter = data.filter((value) => {
-            return value.navn.toLowerCase().includes(searchWord.toLowerCase());
-        });
 
-        if(searchWord === "") {
-            setFilteredData([])
-        } else {
-            setFilteredData(newFilter);
-        }
-    };
-
-    const clearInput = () => {
-        setFilteredData([]);
-        setWordEntered("");
-    }
     return(
         <Autocomplete
             id="free-solo-demo"
@@ -40,28 +20,7 @@ function Search() {
             sx={{ width: 300 }}
             renderInput={(params) => <TextField {...params} label="Søg patient" />}
         />
-        /*
-        <div className="search">
-            <div class="centered" className="searchInput" >
-                <input type="text" placeholder="Indtast patient navn..."
-                       value={wordEntered}
-                       onChange={handleFilter}/>
-                <div className="searchIcon">
-                    {filteredData.length === 0 && wordEntered.length === 0 ? <SearchIcon/> :
-                        <CloseIcon id="clearBtn" onClick={clearInput}/> }
-                </div>
-            </div>
-            {filteredData.length != 0 && (
-                <div className="dataResult">
-                    {filteredData.slice(0,15).map((value, key) => {
-                        return <a className="dataItem">
-                            <p>{value.navn}, {value.cpr} </p>
-                        </a>
-                    })}
-                </div>
-            )}
-        </div>
-        */
+
     )
 }
 
