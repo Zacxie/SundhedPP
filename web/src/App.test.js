a1f20386ca4f6c94f02da9ba09d68ef102c136a6
Zacxie <sebastian.bjerre@pm.me>
2021-11-30 14:44:46 +0100

    Search for patient test - working

--- web/src/App.test.js
+++ web/src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render, screen} from '@testing-library/react';
+import {fireEvent, render, screen, within} from '@testing-library/react';
 import '@testing-library/jest-dom'
 import App from './App';
 import "@testing-library/jest-dom/extend-expect";
@@ -16,7 +16,6 @@ describe("Render components on main page", () => {
 
     test('render mainlayout elements', () => {
         render(<App/>);
-        // expect(screen.getByRole('navigation'));
         expect(screen.getByText(/gustav kirkholt \(s164765\) \- gustav kauman \(s195396\) \- sebastian bjerre \(s163526\) \- mikkel blomsterberg \(s172133\)/i));
         expect(screen.getByRole('grid'));
         expect(screen.getByRole('columnheader', {name: /patient/i}));
@@ -31,31 +30,23 @@ describe("Render components on main page", () => {
 
 })
 
-test('list prescriptions for patient', async () => {
+test('Search for patient', async () => {
     render(<App/>);
-    /*   const autocomplete = screen.getByTestId("autocomplete");
-       const input = within(autocomplete).getByRole("textbox");
-       const delay = ms => new Promise(res => setTimeout(res, ms));
+    const autocomplete = screen.getByTestId("autocomplete");
+    const input = within(autocomplete).getByRole("textbox");
+    const delay = ms => new Promise(res => setTimeout(res, ms));
 
-       autocomplete.click();
-       autocomplete.focus();
+    autocomplete.click();
+    autocomplete.focus();
 
-       fireEvent.change(await input, { target: { value: "Mikkel" } });
-       await delay(2000);
+    fireEvent.change(await input, {target: {value: "Mikkel"}});
+    await delay(1000);
 
+    fireEvent.keyDown(await autocomplete, {key: 'ArrowDown'})
+    fireEvent.keyDown(await autocomplete, {key: 'Enter'})
+    await delay(1000);
 
-       // navigate to the first item in the autocomplete box
-       fireEvent.keyDown(await autocomplete, {key: 'ArrowDown'})
-       fireEvent.keyDown(await autocomplete, { key: 'Enter' })
-       await delay(1000);
-
-       await screen.getByRole('')
-   */
-
-
+    expect(input.value).toEqual("Mikkel Jensen: 170390-2839")
 });
 
-test('renew prescription', () => {
-    render(<App/>);
 
-});
